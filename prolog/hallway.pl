936065b2ea76420f17efd312a58615822bcff186
addariasfr <45012818+addariasfr@users.noreply.github.com>
2020-10-21 17:46:31 -0600

    Added comments

--- prolog/hallway.pl
+++ prolog/hallway.pl
@@ -1,8 +1,12 @@
+/* Establishes the structure of the hallway */
 rooms([lives(_, 1), lives(_, 2), lives(_, 3), lives(_, 4), lives(_, 5)]).
 
+/* Define adjacency as two numbers with an absolute value difference of one */
 nadjacent(W, V) :- 1 =\= abs(W - V).
+/* Define higher as one number greater than the other */
 higher(Y, Z) :- Y > Z.
 
+/* Rules to establish bedroom residents *
 layout(X) :- rooms(X),
 		member(lives(hunter, A), X), A\=5,
 		member(lives(laura, B), X), B\=1,
